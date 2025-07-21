c3487aecb503b472cbd9d04b9874fdaf7629d404
Chong Ving Ming <vmchong123@gmail.com>
2025-07-21 19:57:13 +0800

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,94 @@
+class Cart{
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey){
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage(){
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionsId: '1'
+      },{
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionsId: '2'
+      }];
+    }
+  }
+
+  saveToStorage(){
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+  
+  addToCart(productId, quantity){
+    let matchingItem; 
+
+    this.cartItems.forEach((cartItem)=>{
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    });
+
+    let deliveryOptionsId = '1';
+
+    if(matchingItem){
+      matchingItem.quantity += quantity;
+    } else {
+      this.cartItems.push({productId, quantity, deliveryOptionsId});
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const newCart = [];
+
+    this.cartItems.forEach((cartItem)=>{
+      if(cartItem.productId !== productId){
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionsId){
+    let matchingItem; 
+
+    this.cartItems.forEach((cartItem)=>{
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionsId = deliveryOptionsId;
+
+    this.saveToStorage();
+  }
+
+  calculateCartQuantity(){
+    let cartQuantity = 0;
+
+    this.cartItems.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    return cartQuantity;
+  } 
+}
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
