15abf404e5ac204fc4ba4bd7e867c88b6e52d74e
Chong Ving Ming <vmchong123@gmail.com>
2025-07-22 22:05:40 +0800

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -79,3 +79,15 @@ export function updateDeliveryOption(productId, deliveryOptionsId){
 
   saveToStorage();
 }
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest();
+  
+  xhr.addEventListener('load', ()=>{
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,8 +4,50 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 //import '../data/data/car.js';
 //import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(()=>{
+// array of promises, runs all completely before next code execute
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('value2');
+    });
+  })
+]).then((value)=>{
+  console.log(value);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+//resolve controls 
+new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve('value1');
+  });
+}).then((value) =>{
+  console.log(value);
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+}).then(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+
+// nested 'callbacks' is not good, that's why use promises to keep code flat
+loadProducts(()=>{
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
